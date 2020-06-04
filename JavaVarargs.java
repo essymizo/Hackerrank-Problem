db48837478acf2ea70713991569732ec103f3014
Esther Lalremruati <esther.iitm@gmail.com>
2020-06-05 00:36:22 +0530

    Create JavaVarargs.java

---
+++ JavaVarargs.java
@@ -0,0 +1,9 @@
+ class Add{
+public void add(int x,int y){
+System.out.println(x+"+"+y+"="+(x+y));
+}
+public void add(int x,int y, int z){System.out.println(x+"+"+y+"+"+z+"="+(x+y+z));}
+public void add(int x1,int x2,int x3,int x4,int x5 ){System.out.println(x1+"+"+x2+"+"+x3+"+"+x4+"+"+x5+"="+(x1+x2+x3+x4+x5));}
+public void add(int x1,int x2,int x3,int x4,int x5,int x6)
+{System.out.println(x1+"+"+x2+"+"+x3+"+"+x4+"+"+x5+"+"+x6+"="+(x1+x2+x3+x4+x5+x6));}
+}
